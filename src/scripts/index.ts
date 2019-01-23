0f5a0bbd9f13cd6436445d12bd17c49d6540fb9f
Merlin <loldinis@gmail.com>
2019-01-23 03:57:10 -0200

    v1

---
+++ src/scripts/index.ts
@@ -0,0 +1,10 @@
+/**
+ * App bundle entry point.
+ */
+import "styles/style.css"; // Required to bundle styles!
+
+/*
+ * YOUR CODE HERE
+ */
+import {GameApp} from "app/game-app";
+const gameApp = new GameApp();
